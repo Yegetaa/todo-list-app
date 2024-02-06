0680dfad5aa225e94df6a89d221b480dd5124a99
Yegeta <149103185+Yegetaa@users.noreply.github.com>
2024-02-06 14:40:15 -0500

    Save button done

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,26 +1,60 @@
 import React from 'react';
+import { useState } from 'react';
 
 
   
-function TodoItem({ todo, handleToggleComplete, handleDeleteTodo }) {
-    const {title} = todo;
-    return (
-      <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+/**
+ * @param {{title: string, isComplete: boolean, id: number}} todo
+ * @param {function(string): void} handleSave
+ * @param {function(): void} handleToggleComplete
+ * @param {function(number): void} handleDeleteTodo
+ */
+
+function TodoItem({ todo, dispatch, handleToggleComplete, handleDeleteTodo }) {
+  const {title} = todo;
+  const [showEdit, setShowEdit] = useState(false);
+  const [newTitle, setNewTitle] = useState("");
+
+  /**
+   * @param {Event} e 
+   */
+  const handleSaveClick = (e) => {
+    e.preventDefault();
+    setShowEdit(false);
+    dispatch({ type: 'EDIT_TODO', payload: { id: todo.id, title: newTitle } });
+  };
+
+  return (
+    <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+
+      {showEdit ? (
+        <>
           <input
-          type="checkbox"
-          checked={todo.isComplete}
-          onChange={() => handleToggleComplete(todo.id)}
-        />
-         <h3>{title}</h3>
-        <button
-          onClick={() => handleDeleteTodo(todo.id)}
-          disabled={!todo.isComplete}
-        >
-          Delete
-        </button>
-        <button>Edit</button>
-      </div>
-    );
-  }
+            value={newTitle}
+            onChange={(e) => setNewTitle(e.target.value)}
+          />
+          <button onClick={handleSaveClick}>Save</button>
+        </>
+      ) : (
+        <>
+          <input
+            type="checkbox"
+            checked={todo.isComplete}
+            onChange={() => handleToggleComplete(todo.id)}
+          />
+          <h3>{title}</h3>
+          <button
+            onClick={() => handleDeleteTodo(todo.id)}
+            disabled={!todo.isComplete}
+          >
+            Delete
+          </button>
+          <button onClick={() => setShowEdit(!showEdit)}>Edit</button>
+        </>
+      )}
+
+    </div>
+  );
+}
   
   export default TodoItem;
