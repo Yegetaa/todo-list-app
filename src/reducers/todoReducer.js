67c2b87da4156ba035a39c4f09f0dfbc03d50137
Yegeta <149103185+Yegetaa@users.noreply.github.com>
2024-02-06 14:39:45 -0500

    type: EDIT_TODO done

--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -1,12 +1,47 @@
+/**
+ * Reducer function for managing todo items
+ * @param {object[]} todos - Array of todo items
+ * @param {object} action - Action object
+ * @returns {object[]} New array of todo items
+ */
+
 export function todosReducer(todos, action) {
-    switch (action.type) {
-      case 'TOGGLE_COMPLETE':
-        return todos.map(todo =>
-          todo.id === action.id ? { ...todo, isComplete: !todo.isComplete } : todo
-        );
-      case 'DELETE_TODO':
-        return todos.filter(todo => todo.id !== action.id);
-      default:
-        return todos;
+  switch (action.type) {
+    case 'TOGGLE_COMPLETE':
+      return todos.map(todo => {
+        if (todo.id === action.id) {
+          return {
+            ...todo,
+            isComplete: !todo.isComplete
+          };
+        }
+        return todo;
+      });
+    case 'DELETE_TODO':
+      return todos.filter(todo => todo.id !== action.id);
+
+    case 'EDIT_TODO':
+      return todos.map(todo => {
+        if (todo.id === action.payload.id) {
+          return {
+            ...todo,
+            title: action.payload.title
+          };
+        }
+        return todo;
+      });
+
+    case "ADD_TODO": {
+      return [
+        {
+          id: new Date().getTime(),
+          title: action.payload.title,
+          isCompleted: false
+        },
+        ...todos
+      ];
     }
+    default:
+      return todos;
   }
+}
